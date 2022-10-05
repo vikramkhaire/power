1c3bebf676c5839130d8f08ad5fa53dfac3261d5
vikramkhaire <vickykhaire@gmail.com>
2022-10-05 08:20:46 +0530

    mw mask appending

--- power/hsla_write_mw_masks.py
+++ power/hsla_write_mw_masks.py
@@ -10,9 +10,7 @@ files =  glob.glob(data_path + '/*final*.fits')
 mw_masks_table = tab.Table.read('milky_way_masks.fits')
 mw_wave = mw_masks_table['mask']
 
-#for f in files:
-if 1:
-    f= files[0]
+for f in files:
     data = tab.Table.read(f)
     wave = data['WAVE']
     delta_wave = wave[1]-wave[0]
@@ -21,9 +19,12 @@ if 1:
         diff = np.abs(mw_wave-l)
         min_index = np.argmin(diff)
         if diff[min_index]<2*delta_wave:
-            new_masks.append[7]
-            print(l, mw_wave[min_index], '7')
+            new_masks.append(7)
+            #print(l, mw_wave[min_index], '7')
         else:
-            new_masks.append[0]
+            new_masks.append(0)
+            #print(l, mw_wave[min_index], '0')
+    data.add_column(new_masks, name = 'mask')
+    data.write(f, overwrite= True)
+    print('written', f)
 
-    
