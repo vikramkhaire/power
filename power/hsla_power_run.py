4a81964c8a2b08de8f5c1194ebc6c3d66d4d41c7
vikramkhaire <vickykhaire@gmail.com>
2022-10-10 15:28:32 +0530

    power hsla

---
+++ power/hsla_power_run.py
@@ -0,0 +1,24 @@
+from power.lowz_power import compute_power_hsla
+import astropy.table as tbl
+
+data_path = '/home/vikram/output_power/hsla'
+
+masterfile = data_path  +'/masterfile.fits'
+
+zbinarr = [[0.005, 0.06], [0.06, 0.16], [0.16, 0.26], [0.26, 0.36]]
+waverangearr = [[1050, 1180]]
+
+for zbin in zbinarr:
+    print('working in z=', zbin)
+    for waverange in waverangearr:
+        outpath = '/home/vikram/output_power/hsla_power/'
+        Power_table = tbl.Table(
+            compute_power_hsla(masterfile=masterfile, data_path=data_path, use_metalmasking=True, zbin=zbin,
+                          wavelim=waverange),
+            names=['LS K-modes', 'LS Power(noise and wind corr)', 'N_used', 'C', 'z'])
+        Power_table.add_column(tbl.Column(np.sqrt(np.diag(Power_table['C'])), name='sigma'))
+        out = Power_table['z', 'LS K-modes', 'LS Power(noise and wind corr)', 'sigma', 'N_used']
+        out.write(outpath + 'Power_metals_masked_z{:.2f}-{:.2f}_wave{}-{}.txt'.format(zbin[0], zbin[1], waverange[0],
+                                                                                        waverange[1]),
+                  format="ascii.commented_header", overwrite=True)
+
