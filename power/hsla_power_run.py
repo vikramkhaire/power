d4f26724e86edb2319815ef74bdfa7714c263c78
vikramkhaire <vickykhaire@gmail.com>
2022-10-21 10:22:01 +0530

    rung

--- power/hsla_power_run.py
+++ power/hsla_power_run.py
@@ -12,10 +12,10 @@ waverangearr = [[1050, 1180]]
 for zbin in zbinarr:
     print('working in z=', zbin)
     for waverange in waverangearr:
-        outpath = '/home/vikram/output_power/hsla_power/medium_3.1_quality/'
+        outpath = '/home/vikram/output_power/hsla_power/high_SN'
         Power_table = tbl.Table(
             compute_power_hsla(data_path=data_path, use_metalmasking=True, zbin=zbin, wavelim=waverange, fill_with_noise = False),
-            names=['LS K-modes', 'LS Power(noise and wind corr)', 'N_used', 'C', 'z'])
+            names=['LS K-modes', 'LS Power(noise and wind corr)', 'N_used', 'C', 'z', 'noise_power'])
         Power_table.add_column(tbl.Column(np.sqrt(np.diag(Power_table['C'])), name='sigma'))
         out = Power_table['z', 'LS K-modes', 'LS Power(noise and wind corr)', 'sigma', 'N_used']
         out.write(outpath + 'Power_metals_masked_z{:.2f}-{:.2f}_wave{}-{}.txt'.format(zbin[0], zbin[1], waverange[0],
