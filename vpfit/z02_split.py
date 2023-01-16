f25fa1affba9e70b87a5140ab037d6bb364c5f8d
vikramkhaire <vickykhaire@gmail.com>
2023-01-16 20:11:14 +0530

    splitting on grating G130M

---
+++ vpfit/z02_split.py
@@ -0,0 +1,17 @@
+
+import os
+import matplotlib as mpl
+mpl.use("Agg")
+from enigma.whim.z02_codes.z02_vpfit_forward_models import split
+import numpy as np
+import glob
+os.nice(10)
+
+def run_code(path):
+    split(path)
+    return
+
+path = '/mnt/quasar/vikram/hsla'
+folder = glob.glob(path+ '/gamma*')
+
+split(folder)
